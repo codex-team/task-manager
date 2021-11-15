892e98361633769ab47858d95130e6e79b2e83c6
Taly <vitalik7tv@yandex.ru>
2021-11-15 20:41:31 +0300

    Update path to env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: path.join(__dirname, '/../../.env') });
 
+dotenv.config({ path: path.join(__dirname, '..', '..', '.env') });
 
 /**
  * Config class
