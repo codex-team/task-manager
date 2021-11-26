c56f9d11d0a3eadd9e3bc711dba40dcd567088b1
Taly <vitalik7tv@yandex.ru>
2021-11-26 18:28:38 +0300

    do not build and check front

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -4,10 +4,4 @@ describe('Sample test', function () {
       .its('body')
       .should('have.length.above', 0);
   });
-
-  it('Check frontend entry page', function () {
-    cy.request('http://localhost:3000/')
-      .its('body')
-      .should('have.length.above', 0);
-  });
 });
