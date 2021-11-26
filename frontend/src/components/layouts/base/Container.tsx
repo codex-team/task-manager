bb3694d9b0efbdfd0885847fcd2fbf958c1b23ad
slaveeks <chernyshev2001@icloud.com>
2021-11-26 13:09:26 +0300

    Replaced files to componets/layouts/base, renamed ProjectContainer to Content, added commets, fixed code style, deleted extensions in Props

---
+++ frontend/src/components/layouts/base/Container.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * Interface for container component props
+ */
+interface Props{
+}
+
+/**
+ * Styled container component
+ *
+ * @param props - props of component
+ */
+const ContainerStyled = styled.div<Props>`
+  display: flex;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  background: #FFFFFF;
+`;
+
+/**
+ * Container component
+ *
+ * @param props - props of component
+ */
+const Container: React.FC<Props> = ({ ...props }) => {
+  return (
+    <ContainerStyled {...props}/>
+  );
+};
+
+export default Container;
