0fe726e212754a1b856b124c12931803e53a73eb
Taly <vitalik7tv@yandex.ru>
2021-11-26 18:05:40 +0300

    update host

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('Sample test', function () {
   it('Request', function () {
-    cy.request('http://0.0.0.0:3000/')
+    cy.request('http://localhost:3000/')
       .its('body')
       .should('have.length.above', 0);
   });
