d2353a5f934db223702bcd31cee9479eed98bdc2
Taly <vitalik7tv@yandex.ru>
2021-11-26 18:00:12 +0300

    update port

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('Sample test', function () {
   it('Request', function () {
-    cy.request('http://0.0.0.0:3030/')
+    cy.request('http://0.0.0.0:3000/')
       .its('body')
       .should('have.length.above', 0);
   });
