8bb9f6899ea8e4ad4b3191cf0fe1dfe9adb4031d
Taly <vitalik7tv@yandex.ru>
2021-11-26 18:24:11 +0300

    update build cypress

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,6 +1,12 @@
 describe('Sample test', function () {
-  it('Request', function () {
+  it('Check backend entry page', function () {
     cy.request('http://localhost:3000/api')
+      .its('body')
+      .should('have.length.above', 0);
+  });
+
+  it('Check frontend entry page', function () {
+    cy.request('http://localhost:3000/')
       .its('body')
       .should('have.length.above', 0);
   });
