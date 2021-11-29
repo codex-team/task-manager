0111ecad2cb47f627d1d00dfceb54dab4215e554
Taly <vitalik7tv@yandex.ru>
2021-11-29 15:49:11 +0300

    remove more comments and run failed test

--- e2e/cypress/integration/sample_spec.js
+++ e2e/cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('Sample test', function () {
   it('Check backend entry page', function () {
-    cy.request('/api')
+    cy.request('/apiw')
       .its('body')
       .should('have.length.above', 0);
   });
