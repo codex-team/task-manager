be712caa714a0882d6530aa1776ad430a51dea9b
Taly <vitalik7tv@yandex.ru>
2021-11-29 15:31:18 +0300

    add docker for cypress

--- e2e/cypress/integration/sample_spec.js
+++ e2e/cypress/integration/sample_spec.js
@@ -1,6 +1,6 @@
 describe('Sample test', function () {
   it('Check backend entry page', function () {
-    cy.request('http://localhost:3000/api')
+    cy.request('/api')
       .its('body')
       .should('have.length.above', 0);
   });
