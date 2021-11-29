51e2df94b938cc26852fc1df51797d80b9179f2a
slaveeks <chernyshev2001@icloud.com>
2021-11-29 23:37:04 +0300

    Added storybook for base layouts

---
+++ frontend/src/components/layouts/base/layoutsBase.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Container from './Container';
+import Content from './Content';
+import Sidebar from './Sidebar';
+import ColorVariables from '../../../styles/Colors';
+
+export default {
+  title: 'Example/Layout',
+  component: Container,
+} as ComponentMeta<typeof Container>;
+
+const Template: ComponentStory<typeof Container> = (args) => <Container {...args}><ColorVariables/><Sidebar/><Content/></Container>;
+
+export const Base = Template.bind({});
