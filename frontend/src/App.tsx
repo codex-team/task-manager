6a4119d0a922b69ed9c1d5dd25291d9ffd96683e
slaveeks <chernyshev2001@icloud.com>
2021-11-30 14:20:37 +0300

    Added docs to App.tsx, fixed code style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import Container from './components/layouts/base/Container';
 import Sidebar from './components/layouts/base/Sidebar';
 import Content from './components/layouts/base/Content';
-import ColorVariables from "./styles/Colors";
+import ColorVariables from './styles/Colors';
 
+/**
+ * Makes the main page
+ *
+ * @returns {React.ReactElement}
+ */
 function App(): React.ReactElement {
   return (
     <Container>
