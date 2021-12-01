40a10451b62fd8734a67ab962ab91d33e0b1961e
slaveeks <chernyshev2001@icloud.com>
2021-12-01 21:22:17 +0300

    Some changes in Sidebar.stories

--- app/frontend/src/components/layouts/base/Sidebar.stories.tsx
+++ app/frontend/src/components/layouts/base/Sidebar.stories.tsx
@@ -1,23 +1,25 @@
-import SidebarHeading from "../SidebarHeading";
-import {ComponentMeta, ComponentStory} from "@storybook/react";
-import SidebarTitle from "../../UI/SidebarTitle/SidebarTitle";
-import Button, {Sizes, StyleTypes} from "../../UI/button/Button";
-import React from "react";
-import Sidebar from "./Sidebar";
+import SidebarHeading from '../SidebarHeading';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import SidebarTitle from '../../UI/SidebarTitle/SidebarTitle';
+import Button, {Sizes, StyleTypes} from '../../UI/button/Button';
+import Sidebar from './Sidebar';
 
 export default {
   title: 'Example/Sidebar',
   component: Sidebar,
 } as ComponentMeta<typeof Sidebar>;
 
+
 const Template: ComponentStory<typeof Sidebar> = (args) =>
   <Sidebar>
-    <SidebarHeading {...args}>
-      <SidebarTitle>
-      Codex Tasks
+    <SidebarHeading>
+      <SidebarTitle {...args}>
       </SidebarTitle>
       <Button size={Sizes.SMALL} styleType={StyleTypes.SECONDARY}/>
     </SidebarHeading>
   </Sidebar>;
 
 export const Heading = Template.bind({});
+Heading.args = {
+  children: "Codex Tasks"
+};
