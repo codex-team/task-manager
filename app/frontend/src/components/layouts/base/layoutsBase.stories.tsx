c0cf7fe485f9a3a09ca96998f8383eac02e00724
slaveeks <chernyshev2001@icloud.com>
2021-12-03 12:09:11 +0300

    Added ProjectHeader layout

--- app/frontend/src/components/layouts/base/layoutsBase.stories.tsx
+++ app/frontend/src/components/layouts/base/layoutsBase.stories.tsx
@@ -1,19 +1,21 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import Container from './Container';
-import Content from './Content';
-import Sidebar from './Sidebar';
+import Container from 'components/layouts/base/Container';
+import Content from 'components/layouts/base/Content';
+import Sidebar from 'components/layouts/base/Sidebar';
 import ColorVariables from '../../../styles/Colors';
+import ProjectHeader from 'components/layouts/project/ProjectHeader';
 
 export default {
   title: 'Example/Layout',
   component: Container,
 } as ComponentMeta<typeof Container>;
-
 const Template: ComponentStory<typeof Container> = (args) =>
   <Container {...args}>
     <ColorVariables/>
     <Sidebar/>
-    <Content/>
+    <Content>
+      <ProjectHeader projectTitle={'CodeX Project'}/>
+    </Content>
   </Container>;
 
 export const Base = Template.bind({});
