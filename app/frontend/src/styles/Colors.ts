e981d03024ade9200489a021434025d8cf38c6a6
ilyamore88 <moroz.ilya.olegovich@gmail.com>
2021-12-03 20:56:50 +0300

    add text colors

--- app/frontend/src/styles/Colors.ts
+++ app/frontend/src/styles/Colors.ts
@@ -6,7 +6,15 @@ import { createGlobalStyle } from 'styled-components';
 const ColorVariables = createGlobalStyle`
   :root {
     --color-bg-main: #FFFFFF;
+
+    /**
+     * Text colors variables
+     */
     --color-text-dark: #1D2331;
+    --color-text-gray: #8D8D8D;
+    --color-text-light: #E9F2FF;
+    --color-text-light-secondary: #9CA3AD;
+    --color-text-white: #FFFFFF;
   }
 `;
 
