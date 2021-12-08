b859aed601373e68a7815164b176009325a34a07
slaveeks <chernyshev2001@icloud.com>
2021-12-08 17:22:45 +0300

    Updated card storybook

--- app/frontend/src/components/layouts/card/Card.stories.tsx
+++ app/frontend/src/components/layouts/card/Card.stories.tsx
@@ -9,7 +9,22 @@ export default {
 const Template: ComponentStory<typeof Card> = (args) =>
   <Card {...args}/>;
 
-export const CardWithInfo = Template.bind({});
-CardWithInfo.args = {
+export const WithTaskTitle = Template.bind({});
+WithTaskTitle.args = {
   taskTitle: 'My task',
+};
+
+export const WithTitleAndCompleteness = Template.bind({});
+WithTitleAndCompleteness.args = {
+  taskTitle: 'My task',
+  subtasksNumber: 10,
+  completedSubtasks: 4,
+};
+
+export const WithAllInfo = Template.bind({});
+WithAllInfo.args = {
+  taskTitle: 'My task',
+  subtasksNumber: 10,
+  completedSubtasks: 4,
+  assigneesNumber: 3,
 };
