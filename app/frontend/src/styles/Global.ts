22f62eee28fb40091e531779f3a2b6f493589605
Ilya Moroz <37909603+ilyamore88@users.noreply.github.com>
2021-12-08 16:43:59 +0300

    [Feature] Add text colors (#62)

--- app/frontend/src/styles/Global.ts
+++ app/frontend/src/styles/Global.ts
@@ -9,7 +9,7 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    color: var(--color-text-dark);
+    color: var(--color-text-primary);
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   }
 
