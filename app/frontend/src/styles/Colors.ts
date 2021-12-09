b5f0a20c1f224bbec9125acc8b9dd3194f9adaac
slaveeks <chernyshev2001@icloud.com>
2021-12-09 19:12:10 +0300

    Changed names

--- app/frontend/src/styles/Colors.ts
+++ app/frontend/src/styles/Colors.ts
@@ -5,9 +5,18 @@ import { createGlobalStyle } from 'styled-components';
  */
 const ColorVariables = createGlobalStyle`
   :root {
-    --color-accent: #387CE1;
+    --color-white: #FFFFFF;
+    --color-gray-1: #F4F4F4;
+    --color-gray-2: #EBEBEB;
+    --color-gray-3: #DBDBDB;
+    --color-gray-4: #D6D6D6;
+    --color-gray-5: #8D8D8D;
+    --color-gray-6: #1D2331;
 
     --color-bg-main: #FFFFFF;
+    --color-text-dark: var(--color-gray-6);
+    --color-accent: #387CE1;
+
 
     /**
      * Text colors variables
@@ -20,10 +29,10 @@ const ColorVariables = createGlobalStyle`
     /**
     * Card component colors variables
     */
-    --color-card-border: #F4F4F4;
-    --color-card-border-hover: #D6D6D6;
-    --color-card-border-active: #1D2331;
-    --color-card-bg-active: #1D2331;
+    --color-line: #F4F4F4;
+    --color-line-hover: #D6D6D6;
+    --color-line-active: #1D2331;
+    --color-bg-active: #1D2331;
 
   }
 `;
