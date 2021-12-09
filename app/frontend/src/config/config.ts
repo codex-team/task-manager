9de03187d242517e0d2bb2b157b0ddc5e7389637
Aleksey <ziqunion95@gmail.com>
2021-12-09 18:05:05 +0300

    add default token

--- app/frontend/src/config/config.ts
+++ app/frontend/src/config/config.ts
@@ -10,5 +10,5 @@ export class Config {
   /**
    * Personal token
    */
-  public static token: string = process.env.token || '?';
+  public static token: string = process.env.token || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 }
