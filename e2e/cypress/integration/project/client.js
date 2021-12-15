9c4606feb9608c4b13b04186e337f0d948135abc
Taly <vitalik7tv@yandex.ru>
2021-12-15 16:12:22 +0300

    remove logs

--- e2e/cypress/integration/project/client.js
+++ e2e/cypress/integration/project/client.js
@@ -10,21 +10,23 @@ describe('Ctproto API', function () {
         token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
       },
       onAuth: (data) => {
-        cy.log('Authorization is success', data);
+        // cy.log('Authorization is success', data);
       },
       onMessage: (data) => {
-        cy.log('Incoming message: ', data);
+        // cy.log('Incoming message: ', data);
       },
     });
   })
 
-  it('Get all projects', function () {
-    client
+  it('Get all projects', async function () {
+    const response = await client
       .send('get-projects', {
         workspaceId: 'test-test-test',
-      })
-      .then(function (responsePayload) {
-        expect(responsePayload).ownProperty('projects');
-      })
+      });
+
+    // cy.log('response', JSON.stringify(response));
+
+    expect(response, 'Response').to.have.key('projects');
+    expect(response.projects.length, 'Number of projects').to.eq(2);
   });
 });
