16f07027e92f9358d12e2db4bfbb1acf7a226efc
slaveeks <chernyshev2001@icloud.com>
2021-12-16 00:06:45 +0300

    Item of project list moved to sidebar in storybook

--- app/frontend/src/components/UI/project-list/components/ProjectListItem.stories.tsx
+++ app/frontend/src/components/UI/project-list/components/ProjectListItem.stories.tsx
@@ -1,12 +1,15 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import ProjectListItem from 'components/UI/project-list/components/ProjectListItem';
+import Sidebar from 'components/layouts/base/Sidebar';
 
 export default {
   component: ProjectListItem,
 } as ComponentMeta<typeof ProjectListItem>;
 
 const Template: ComponentStory<typeof ProjectListItem> = (args) =>
-  <ProjectListItem {...args}/>;
+  <Sidebar>
+    <ProjectListItem {...args}/>
+  </Sidebar>;
 
 export const WithTitle = Template.bind({});
 WithTitle.args = {
