86525d53472be414bb52ec8f93540ca1ab6fba1a
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2021-12-16 20:04:02 +0200

    Rename utils to transport

--- app/frontend/src/services/projects.ts
+++ app/frontend/src/services/projects.ts
@@ -1,6 +1,6 @@
 import { Project } from 'types/entities';
 import { CreateProjectResponsePayload } from 'types/transport/responses/project/create';
-import client from 'utils/ctproto-client';
+import client from 'transport/ctproto-client';
 
 /**
  * Creates new project
