4452a5c2c6267fcd2b08bd414d09566699faa5f8
slaveeks <81693471+slaveeks@users.noreply.github.com>
2021-12-17 16:03:10 +0300

    Added project list (#73)

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import Container from './components/layouts/base/Container';
-import Sidebar from './components/layouts/base/Sidebar';
-import Content from './components/layouts/base/Content';
+import Container from 'components/layouts/base/Container';
+import Sidebar from 'components/layouts/base/Sidebar';
+import Content from 'components/layouts/base/Content';
 import ColorVariables from './styles/Colors';
 import GlobalStyles from './styles/Global';
+import ProjectList from 'components/UI/project-list/ProjectList';
+import ProjectListItem from 'components/UI/project-list/ProjectListItem';
+import SidebarHeader from 'components/layouts/base/SidebarHeader';
 
 /**
  * Makes the main page
@@ -15,7 +18,14 @@ function App(): React.ReactElement {
     <Container>
       <ColorVariables/>
       <GlobalStyles/>
-      <Sidebar/>
+      <Sidebar>
+        <SidebarHeader sidebarTitle={'CodeX Tasks'}/>
+        <ProjectList>
+          <ProjectListItem title={'All projects'} />
+          <ProjectListItem title={'Hawk'} picture={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoHDBRoAMoZ_mga_JVM_Y1drzCF3yII75v4-gqJdueV-hBbhdOPOmAVedy3CbKUWqDfSs&usqp=CAU'}/>
+          <ProjectListItem title={'Github Bot'} picture={'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'}/>
+        </ProjectList>
+      </Sidebar>
       <Content/>
     </Container>
   );
---
+++ app/frontend/src/components/UI/project-list/ProjectList.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import ProjectList from 'components/UI/project-list/ProjectList';
+import ProjectListItem from 'components/UI/project-list/ProjectListItem';
+import Sidebar from 'components/layouts/base/Sidebar';
+
+export default {
+  component: ProjectList,
+} as ComponentMeta<typeof ProjectList>;
+
+export const Empty: ComponentStory<typeof ProjectList> = (args) => <ProjectList {...args} />;
+
+export const OneItem: ComponentStory<typeof ProjectList> = (args) => (
+  <Sidebar>
+    <ProjectList {...args}>
+      <ProjectListItem title={'vc'} picture={'https://vc.ru/cover/fb/general/cover.jpg'}/>
+    </ProjectList>
+  </Sidebar>
+);
+
+export const ManyItems: ComponentStory<typeof ProjectList> = (args) => (
+  <Sidebar>
+    <ProjectList {...args}>
+      <ProjectListItem title={'All projects'} />
+      <ProjectListItem title={'Hawk'} picture={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoHDBRoAMoZ_mga_JVM_Y1drzCF3yII75v4-gqJdueV-hBbhdOPOmAVedy3CbKUWqDfSs&usqp=CAU'}/>
+      <ProjectListItem title={'Github Bot'} picture={'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'}/>
+    </ProjectList>
+  </Sidebar>
+);
+
