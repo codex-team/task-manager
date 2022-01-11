a9689b3d36bb74d5b1ef210d277c46a3c7f13967
slaveeks <chernyshev2001@icloud.com>
2022-01-11 20:14:33 +0300

    Changed some styles

--- app/frontend/src/components/layouts/popup/Popup.tsx
+++ app/frontend/src/components/layouts/popup/Popup.tsx
@@ -48,15 +48,18 @@ const Popup: React.FC<Props> = ({ children, backDropClick }) => {
  * Styled popup component
  */
 const PopupStyled = styled.div`
+  padding-top: 20px;
+  padding-bottom: 20px;
   width: 100%;
-  height: 100%;
-  position: fixed;
+  min-height: 100%;
+  position: absolute;
   display: flex;
   align-items: center;
   justify-content: center;
   top: 0;
   left: 0;
   background-color: rgba(0,0,0,0.2);
+  overflow: scroll;
 `;
 
 export default Popup;
