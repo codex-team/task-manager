9231195797a86f902538cf222da52add0330fd4a
slaveeks <chernyshev2001@icloud.com>
2022-01-11 20:17:42 +0300

    Added links for card

---
+++ app/frontend/src/components/UI/card/CardLink.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import { Link, LinkProps, useMatch, useResolvedPath } from 'react-router-dom';
+import Card, { Props as CardProps, TaskProps } from 'components/UI/card/Card';
+
+/**
+ * Link wrapper for card component.
+ * Sets isActive state of the card based on current location
+ *
+ * @param props - props of the component
+ */
+const CardLink: React.FC<LinkProps & CardProps & TaskProps > = ({ children, to, ...props }) => {
+  const resolved = useResolvedPath(to);
+  const match = useMatch({
+    path: resolved.pathname,
+    end: true,
+  });
+
+  return (
+    <div>
+      <StyledLink
+        to={ to }
+        { ...props }
+      >
+        <Card projectInfo={props.projectInfo} status={props.status} taskTitle={props.taskTitle} isActive={!!match} task={props.task} setTask={props.setTask}/>
+      </StyledLink>
+    </div>
+  );
+};
+
+/**
+ * Link component with overriden styles
+ */
+const StyledLink = styled(Link)`
+  text-decoration: unset;
+`;
+
+export default CardLink;
