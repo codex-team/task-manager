edd857242e9ef3df6781de37b47e0b6ceb670114
Tanya Fomina <fomina.tatianaaa@yandex.ru>
2022-01-31 22:57:22 +0300

    BE methods cleanup

--- app/backend/src/ui/methods/projects/create.ts
+++ app/backend/src/ui/methods/projects/create.ts
@@ -8,6 +8,20 @@ import { createStatus } from '../statuses/create-status';
 const PREDEFINED_STATUSES = ['To do', 'In progress', 'Done'];
 
 /**
+ * Creates standard predefined statuses for project with specified id
+ *
+ * @param projectId - id of the project statuses are created for
+ */
+async function createStandardStatuses(projectId: string): Promise<void> {
+  for (const label of PREDEFINED_STATUSES) {
+    await createStatus({
+      label,
+      projectId: projectId,
+    });
+  }
+}
+
+/**
  * Creates new project
  *
  * @param title - project title
@@ -21,12 +35,7 @@ export async function createProject(title: string, picture?: string, messengerCh
     messengerChannelUrl,
   });
 
-  for (const label of PREDEFINED_STATUSES) {
-    await createStatus({
-      label,
-      projectId: newProject.toObject()._id,
-    });
-  }
+  await createStandardStatuses(newProject.toObject()._id);
 
   return newProject;
 }
--- app/backend/src/ui/methods/task/update-task.ts
+++ app/backend/src/ui/methods/task/update-task.ts
@@ -13,9 +13,17 @@ export async function updateTask(data: UpdateTaskPayload): Promise<Task | null> 
 
   const task = await TaskModel.findOneAndUpdate(query, data, { new: true }).exec();
 
-  if (!task || !task.statusId) {
+  if (!task) {
     return null;
   }
+
+  if (!task.statusId) {
+    return {
+      ...(task.toObject()),
+      status: null,
+    };
+  }
+
   // Add expanded status data (if any) to result object
   const status = await getStatusById(task.statusId);
 
