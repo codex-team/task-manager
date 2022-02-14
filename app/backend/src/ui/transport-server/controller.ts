fc8f0d66b493b21f569bb7a57ba2096652ee8ed5
Umang G. Patel <23169768+robonetphy@users.noreply.github.com>
2022-02-14 21:20:24 +0530

    backend update methods added

--- app/backend/src/ui/transport-server/controller.ts
+++ app/backend/src/ui/transport-server/controller.ts
@@ -1,5 +1,6 @@
 import { ApiRequest, ApiResponse } from '../../../../types/transport';
 import { createProject } from '../methods/projects/create';
+import { updateProject } from '../methods/projects/update';
 import { getProjects } from '../methods/projects/get-projects';
 import { createTask } from '../methods/task/create';
 import { getTasks } from '../methods/task/get-tasks';
@@ -26,6 +27,14 @@ export async function handleMessage(message: ApiRequest): Promise<ApiResponse['p
         message.payload.messengerChannelUrl
       ),
     };
+    case 'update-project': return {
+      project: await updateProject(
+        message.payload.id,
+        message.payload.title,
+        message.payload.picture,
+        message.payload.messengerChannelUrl
+      ),
+    };
     case 'get-projects': return {
       projects: await getProjects(),
     };
@@ -42,8 +51,9 @@ export async function handleMessage(message: ApiRequest): Promise<ApiResponse['p
       tasks: await getTasks(message.payload.projectId),
     };
     case 'get-task-by-id': return {
-      task: await getTaskById(message.payload.taskId) }
-    ;
+      task: await getTaskById(message.payload.taskId)
+    }
+      ;
     case 'update-task': return {
       task: await updateTask(message.payload),
     };
