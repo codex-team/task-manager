44f1d23f405e177dbde074089cc9b6c0b852f10a
Umang G. Patel <23169768+robonetphy@users.noreply.github.com>
2022-03-16 22:56:20 +0530

    fix: update the project response payload

--- app/frontend/src/services/projects.ts
+++ app/frontend/src/services/projects.ts
@@ -2,7 +2,8 @@ import client from 'transport/ctproto-client';
 import { CreateProjectResponsePayload } from 'types/transport/responses/project/create-project';
 import { GetProjectsMessagePayload } from 'types/transport/requests/project/get-projects';
 import { GetProjectsResponsePayload } from 'types/transport/responses/project/get-projects';
-import { UpdateProjectPayload } from 'types/transport/requests/project/update-project';
+import { UpdateProjectMessagePayload } from 'types/transport/requests/project/update-project';
+import { UpdateProjectResponsePayload } from 'types/transport/responses/project/update-project';
 import { CreateProjectMessagePayload } from 'types/transport/requests/project/create-project';
 
 /**
@@ -21,10 +22,10 @@ export async function createProject(data: CreateProjectMessagePayload): Promise<
  *
  * @param data - updated project data
  */
-export async function updateProject(data: UpdateProjectPayload): Promise<CreateProjectResponsePayload> {
+export async function updateProject(data: UpdateProjectMessagePayload): Promise<UpdateProjectResponsePayload> {
   const response = await client.send('update-project', data);
 
-  return response as CreateProjectResponsePayload;
+  return response as UpdateProjectResponsePayload;
 }
 
 
